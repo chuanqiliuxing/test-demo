679df9a7fb27fe0721b4ce57fd80f70b0faacd07
chuanqiliuxing <545243316@qq.com>
2021-01-28 22:27:43 +0800

    提交Hello文件

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello{
+    public static void name() {
+        System.out.println("王某某");
+    }
+    public static void main(String[] args) {
+        name();
+        System.out.println("结束了");
+    }
+}
